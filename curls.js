781b3e379d3fa646ae7044349ff3d1173466d9c4
eyevent <eyevent@USSEAGirls21.ey.net>
2018-07-25 14:00:53 -0700

    adding all files

---
+++ curls.js
@@ -0,0 +1,45 @@
+
+
+function bigImg(x) {
+    document.getElementById("braids").width = "300";
+    document.getElementById("braids").height = "300";
+}
+
+
+function smallImg(x) {
+    document.getElementById("braids").width = "200";
+    document.getElementById("braids").height = "200";
+}
+
+function bigImg2(x) {
+    document.getElementById("cornRows").width = "300";
+    document.getElementById("cornRows").height = "300";
+}
+
+
+function smallImg2(x) {
+    document.getElementById("cornRows").width = "200";
+    document.getElementById("cornRows").height = "200";
+}
+
+function bigImg3(x) {
+    document.getElementById("washnGo").width = "300";
+    document.getElementById("washnGo").height = "300";
+}
+
+
+function smallImg3(x) {
+    document.getElementById("washnGo").width = "200";
+    document.getElementById("washnGo").height = "200";
+}
+
+function bigImg4(x) {
+    document.getElementById("locs").width = "300";
+    document.getElementById("locs").height = "300";
+}
+
+
+function smallImg4(x) {
+    document.getElementById("locs").width = "200";
+    document.getElementById("locs").height = "200";
+}
